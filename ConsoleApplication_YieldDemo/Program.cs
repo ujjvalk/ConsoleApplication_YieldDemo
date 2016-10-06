61677850747e21975d5e51c8f97a69fe785b36bb
ujjvalk <ujjvalk@gmail.com>
2016-10-06 14:46:10 +0530

    Add project files.

---
+++ ConsoleApplication_YieldDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_YieldDemo
+{
+    class Program
+    {
+        static List<long> Number = new List<long>();
+
+        static List<long> SimpleLoop()
+        {
+            for (var i = 10000000; i < 20000000; i++)
+            {
+                Number.Add(i);
+            }
+            return Number;
+        }
+
+        static IEnumerable<long> YieldLoop()
+        {
+            foreach (long i in Number)
+            {
+                if (i % 2==0) 
+                    yield return i;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            SimpleLoop();
+            IEnumerable<long> yieldNumbers = YieldLoop();
+            foreach (var item in yieldNumbers)
+            {
+                Console.Write(item); Console.Write(item); Console.Write(item); Console.Write(item); Console.Write(item); 
+                Console.Write(item); Console.Write(item); Console.Write(item); Console.Write(item); Console.Write(item); 
+                Console.Write(item); Console.Write(item); Console.Write(item); Console.Write(item); Console.Write(item); 
+                Console.Write(item); Console.Write(item); Console.Write(item); Console.Write(item); Console.Write(item); 
+                Console.Write(item); Console.Write(item); Console.Write(item); Console.Write(item); Console.Write(item); 
+                Console.Write(item); Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
